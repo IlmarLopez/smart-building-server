98b9bd660191c2f8f03381e7f10c4c952c2ece63
Ilmar López <ilmarlopezr@hotmail.com>
2019-11-27 02:15:33 -0800

    Return with format json list of alarms

---
+++ src/com/ilmarlopez/smart/building/server/AlarmListServlet.java
@@ -0,0 +1,70 @@
+package com.ilmarlopez.smart.building.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.ilmarlopez.smart.building.beans.Alarm;
+import com.ilmarlopez.smart.building.utils.DBUtils;
+import com.ilmarlopez.smart.building.utils.MyUtils;
+
+/**
+ * Servlet implementation class AlarmListServlet
+ */
+@WebServlet("/alarmListServlet")
+public class AlarmListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Gson gson = new Gson();
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AlarmListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection conn = MyUtils.getStoredConnection(request);
+		String errorString = null;
+        List<Alarm> list = null;
+        try {
+            list = DBUtils.queryAlarm(conn);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            errorString = e.getMessage();
+        }
+        
+        // Store info in request attribute, before forward to views
+        request.setAttribute("errorString", errorString);
+        request.setAttribute("productList", list);
+        
+        String employeeJsonString = this.gson.toJson(list);
+        PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(employeeJsonString);
+        out.flush();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
